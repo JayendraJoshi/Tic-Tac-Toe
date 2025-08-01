1d8b4734a94f49e04c89ce36035d00f5295fd24c
Jayendra Lal Joshi <Jayendralal.joshi@nielseniq.com>
2025-08-01 20:04:02 +0200

    Refactor code,create all elements dynamically, add newRoundButton, improve styling remove unnecessary commented code let all elements be rendered inside 'renderElements' for consistency add function'getFirstPlayer' to let first player start new rounds replace 'handleDisplayDiv' module with simple function as only one function is required add newRoundButton in 'renderElements' and create eventListener in 'handleGameControl' let 'startDialog' display the first player instead of the active player so every round starts with player 1 use new background, font and border-colors for index.css and game.css apply styles for playerScore elements, buttons in game.css improve font-color, spacing, font-sizes in game.css

--- gameFiles/game.js
+++ gameFiles/game.js
@@ -2,7 +2,7 @@ function createBoardCell() {
   let div = document.createElement("div");
 
   (function styleDiv() {
-    div.style.border = "1px solid black";
+    div.style.border = "1px solid #F5CB5C";
     div.style.flexGrow = "1";
     div.style.width = "100%";
     div.style.display = "flex";
@@ -21,6 +21,7 @@ function createBoardCell() {
 const renderElements = (function () {
   const player1Name = sessionStorage.getItem("player1Name");
   const player2Name = sessionStorage.getItem("player2Name");
+  const main = document.querySelector("main");
   (function renderBoard() {
     const divContainer = document.createElement("div");
     divContainer.classList.add("div-container");
@@ -38,10 +39,9 @@ const renderElements = (function () {
       divRowContainer.classList.add("row-container");
       divContainer.appendChild(divRowContainer);
     }
-    const main = document.querySelector("main");
     main.appendChild(divContainer);
   })();
-  (function renderPlayer1ScoreContainer(){
+  (function renderPlayer1ScoreContainer() {
     const player1ScoreDiv = document.querySelector(".player1ScoreContainer");
     const player1H2 = document.createElement("h2");
     player1H2.textContent = player1Name;
@@ -52,34 +52,34 @@ const renderElements = (function () {
 
     const title = document.createElement("h2");
     title.classList.add("scoreTitle");
-    title.textContent="Score";
+    title.textContent = "Score";
 
     const score = document.createElement("div");
     score.classList.add("scoreCounter");
-    score.textContent="0";
+    score.textContent = "0";
 
     scoreDiv.appendChild(title);
     scoreDiv.appendChild(score);
 
     player1ScoreDiv.appendChild(player1H2);
-    player1ScoreDiv.appendChild(scoreDiv); 
+    player1ScoreDiv.appendChild(scoreDiv);
   })();
-  (function renderPlayer2ScoreContainer(){
+  (function renderPlayer2ScoreContainer() {
     const player2ScoreDiv = document.querySelector(".player2ScoreContainer");
     const player2H2 = document.createElement("h2");
     player2H2.textContent = player2Name;
-    player2H2.classList.add("playe2Name");
+    player2H2.classList.add("player2Name");
 
     const scoreDiv = document.createElement("div");
-    scoreDiv.classList.add("scoreCounterContainer2")
+    scoreDiv.classList.add("scoreCounterContainer2");
 
     const title = document.createElement("h2");
     title.classList.add("scoreTitle");
-    title.textContent="Score";
+    title.textContent = "Score";
 
     const score = document.createElement("div");
     score.classList.add("scoreCounter");
-    score.textContent="0";
+    score.textContent = "0";
 
     scoreDiv.appendChild(title);
     scoreDiv.appendChild(score);
@@ -87,22 +87,41 @@ const renderElements = (function () {
     player2ScoreDiv.appendChild(player2H2);
     player2ScoreDiv.appendChild(scoreDiv);
   })();
+  (function renderGameDisplay() {
+    const displayContainer = document.createElement("div");
+    displayContainer.classList.add("display");
+    main.appendChild(displayContainer);
+  })();
+  (function renderReturnButton() {
+    const returnButton = document.createElement("button");
+    returnButton.classList.add("returnButton");
+    returnButton.textContent = "Return";
+    main.appendChild(returnButton);
+  })();
+  (function renderNewRoundButton() {
+    const newRoundButton = document.createElement("button");
+    newRoundButton.textContent = "New Round";
+    newRoundButton.classList.add("newRoundButton");
+    newRoundButton.classList.add("invisible");
+    main.appendChild(newRoundButton);
+  })();
 })();
-
-
 function createPlayer(name, token) {
   return { name, token };
 }
 const handlePlayers = function (player1, player2) {
   const players = [player1, player2];
-  const divDisplayer = handleDisplayDiv();
+  const divDisplay = getDisplayDiv();
   let activePlayer = players[0];
+  function getFirstPlayer() {
+    return players[0];
+  }
   function getActivePlayer() {
     return activePlayer;
   }
   function setActivePlayer(player) {
     activePlayer = player;
-    divDisplayer.displayDiv.textContent=`${activePlayer.name}'s turn (${activePlayer.token})`;
+    divDisplay.textContent = `${activePlayer.name}'s turn (${activePlayer.token})`;
   }
   function switchPlayerTurn() {
     if (getActivePlayer() === players[0]) {
@@ -112,20 +131,20 @@ const handlePlayers = function (player1, player2) {
     }
   }
   function displayWinner() {
-    console.log(`${getActivePlayer().name} has won!`);
-    divDisplayer.displayDiv.textContent=`${getActivePlayer().name} has won!`;
+    divDisplay.textContent = `${getActivePlayer().name} has won!`;
   }
   return {
     getActivePlayer,
     setActivePlayer,
     switchPlayerTurn,
-    displayWinner
+    displayWinner,
+    getFirstPlayer,
   };
 };
 const handleGameLogic = function () {
   const cells = document.querySelectorAll(".cell");
   const playersHandler = handlePlayers(player1, player2);
-  const divDisplayer = handleDisplayDiv();
+  const divDisplay = getDisplayDiv();
   function getArrayFromCells() {
     const cellsArray = [];
     for (let i = 0; i < 3; i++) {
@@ -147,122 +166,90 @@ const handleGameLogic = function () {
         }
       }
     }
-    divDisplayer.displayDiv.textContent="No empty cells left, it's a tie!";
+    divDisplay.textContent = "No empty cells left, it's a tie!";
     console.log("No empty cells left, it's a tie!");
     return false;
   }
-  function isTheCellAlreadyMarked(div){
-    if(div.textContent!=""){
+  function isTheCellAlreadyMarked(div) {
+    if (div.textContent != "") {
       return true;
     }
-
   }
   function hasSomeoneWon() {
     const boardWithCellValues = getArrayFromCells();
     if (
-      boardWithCellValues[0][0] === "X" &&
-      boardWithCellValues[0][1] === "X" &&
-      boardWithCellValues[0][2] === "X"
-     ||
-    (
-      boardWithCellValues[0][0] === "O" &&
-      boardWithCellValues[0][1] === "O" &&
-      boardWithCellValues[0][2] === "O"
-    ) ||
-    (
-      boardWithCellValues[0][0] === "X" &&
-      boardWithCellValues[1][0] === "X" &&
-      boardWithCellValues[2][0] === "X"
-    ) ||
-    (
-      boardWithCellValues[0][0] === "O" &&
-      boardWithCellValues[1][0] === "O" &&
-      boardWithCellValues[2][0] === "O"
-    )|| 
-    (
-      boardWithCellValues[0][0] === "X" &&
-      boardWithCellValues[1][1] === "X" &&
-      boardWithCellValues[2][2] === "X"
-    )||
-    (
-      boardWithCellValues[0][0] === "O" &&
-      boardWithCellValues[1][1] === "O" &&
-      boardWithCellValues[2][2] === "O"
-    ) ||
-    (
-      boardWithCellValues[0][1] === "X" &&
-      boardWithCellValues[1][1] === "X" &&
-      boardWithCellValues[2][1] === "X"
-    ) ||
-    (
-      boardWithCellValues[0][1] === "O" &&
-      boardWithCellValues[1][1] === "O" &&
-      boardWithCellValues[2][1] === "O"
-    ) ||
-    (
-      boardWithCellValues[0][2] === "X" &&
-      boardWithCellValues[1][1] === "X" &&
-      boardWithCellValues[2][0] === "X"
-    ) ||
-    (
-      boardWithCellValues[0][2] === "O" &&
-      boardWithCellValues[1][1] === "O" &&
-      boardWithCellValues[2][0] === "O"
-    ) ||
-    (
-      boardWithCellValues[0][2] === "X" &&
-      boardWithCellValues[1][2] === "X" &&
-      boardWithCellValues[2][2] === "X"
-    ) ||
-    (
-      boardWithCellValues[0][2] === "O" &&
-      boardWithCellValues[1][2] === "O" &&
-      boardWithCellValues[2][2] === "O"
-    ) ||
-    (
-      boardWithCellValues[1][0] === "X" &&
-      boardWithCellValues[1][1] === "X" &&
-      boardWithCellValues[1][2] === "X"
-    ) ||
-     (
-      boardWithCellValues[1][0] === "O" &&
-      boardWithCellValues[1][1] === "O" &&
-      boardWithCellValues[1][2] === "O"
-    ) ||
-    (
-      boardWithCellValues[2][0] === "X" &&
-      boardWithCellValues[2][1] === "X" &&
-      boardWithCellValues[2][2] === "X"
-    ) ||
-    (
-      boardWithCellValues[2][0] === "O" &&
-      boardWithCellValues[2][1] === "O" &&
-      boardWithCellValues[2][2] === "O"
-    )) {
+      (boardWithCellValues[0][0] === "X" &&
+        boardWithCellValues[0][1] === "X" &&
+        boardWithCellValues[0][2] === "X") ||
+      (boardWithCellValues[0][0] === "O" &&
+        boardWithCellValues[0][1] === "O" &&
+        boardWithCellValues[0][2] === "O") ||
+      (boardWithCellValues[0][0] === "X" &&
+        boardWithCellValues[1][0] === "X" &&
+        boardWithCellValues[2][0] === "X") ||
+      (boardWithCellValues[0][0] === "O" &&
+        boardWithCellValues[1][0] === "O" &&
+        boardWithCellValues[2][0] === "O") ||
+      (boardWithCellValues[0][0] === "X" &&
+        boardWithCellValues[1][1] === "X" &&
+        boardWithCellValues[2][2] === "X") ||
+      (boardWithCellValues[0][0] === "O" &&
+        boardWithCellValues[1][1] === "O" &&
+        boardWithCellValues[2][2] === "O") ||
+      (boardWithCellValues[0][1] === "X" &&
+        boardWithCellValues[1][1] === "X" &&
+        boardWithCellValues[2][1] === "X") ||
+      (boardWithCellValues[0][1] === "O" &&
+        boardWithCellValues[1][1] === "O" &&
+        boardWithCellValues[2][1] === "O") ||
+      (boardWithCellValues[0][2] === "X" &&
+        boardWithCellValues[1][1] === "X" &&
+        boardWithCellValues[2][0] === "X") ||
+      (boardWithCellValues[0][2] === "O" &&
+        boardWithCellValues[1][1] === "O" &&
+        boardWithCellValues[2][0] === "O") ||
+      (boardWithCellValues[0][2] === "X" &&
+        boardWithCellValues[1][2] === "X" &&
+        boardWithCellValues[2][2] === "X") ||
+      (boardWithCellValues[0][2] === "O" &&
+        boardWithCellValues[1][2] === "O" &&
+        boardWithCellValues[2][2] === "O") ||
+      (boardWithCellValues[1][0] === "X" &&
+        boardWithCellValues[1][1] === "X" &&
+        boardWithCellValues[1][2] === "X") ||
+      (boardWithCellValues[1][0] === "O" &&
+        boardWithCellValues[1][1] === "O" &&
+        boardWithCellValues[1][2] === "O") ||
+      (boardWithCellValues[2][0] === "X" &&
+        boardWithCellValues[2][1] === "X" &&
+        boardWithCellValues[2][2] === "X") ||
+      (boardWithCellValues[2][0] === "O" &&
+        boardWithCellValues[2][1] === "O" &&
+        boardWithCellValues[2][2] === "O")
+    ) {
       return true;
     } else {
       return false;
     }
   }
-
   return {
     getArrayFromCells,
     areThereCellsLeft,
     hasSomeoneWon,
     isTheCellAlreadyMarked,
-    cells
+    cells,
   };
 };
 const handleGameControl = function (player1, player2) {
   const playerHandler = handlePlayers(player1, player2);
   const gameLogicHandler = handleGameLogic();
-  const divDisplayer = handleDisplayDiv();
-  /*const handleHomeButton = handleHomeAndResetButton().showHomeButton;*/
+  const divDisplay = getDisplayDiv();
+  const newRoundButton = document.querySelector(".newRoundButton");
 
   const handleEventListeners = function () {
     function handleCellClick(event) {
       const div = event.target;
-      if(gameLogicHandler.isTheCellAlreadyMarked(div)){
+      if (gameLogicHandler.isTheCellAlreadyMarked(div)) {
         return;
       }
       playRound(div);
@@ -280,21 +267,29 @@ const handleGameControl = function (player1, player2) {
       });
     }
 
-    function setClickEventOnReturnButton(){
-      const returnButton = document.querySelector(".return");
-      returnButton.addEventListener("click",function(){
+    function setClickEventOnReturnButton() {
+      const returnButton = document.querySelector(".returnButton");
+      returnButton.addEventListener("click", function () {
         document.body.classList.add("fade-out");
         setTimeout(() => {
-        window.location.href = "index.html"; 
+          window.location.href = "index.html";
         }, 300);
-      })
+      });
     }
 
-    function setClickEventOnNewRoundButton(){
-
+    function setClickEventOnNewRoundButton() {
+      newRoundButton.addEventListener("click", function () {
+        gameLogicHandler.cells.forEach((div) => {
+          div.textContent = "";
+          div.addEventListener("click", handleCellClick);
+        });
+        newRoundButton.classList.add("invisible");
+        startDialog();
+      });
     }
     setClickEventOnReturnButton();
     setClickEventOnCells();
+    setClickEventOnNewRoundButton();
     return {
       handleCellClick,
       setClickEventOnCells,
@@ -303,87 +298,56 @@ const handleGameControl = function (player1, player2) {
   };
   const eventHandler = handleEventListeners();
 
-  function increaseCounter(){
+  function increaseCounter() {
     const winner = playerHandler.getActivePlayer();
-    if(winner.name===document.querySelector(".player1Name").textContent){
-      const scoreCounter= document.querySelector(".player1ScoreContainer > .scoreCounterContainer1 > .scoreCounter");
+    if (winner.name === document.querySelector(".player1Name").textContent) {
+      const scoreCounter = document.querySelector(
+        ".player1ScoreContainer > .scoreCounterContainer1 > .scoreCounter"
+      );
+      scoreCounter.textContent = parseInt(scoreCounter.textContent) + 1;
+    } else {
+      const scoreCounter = document.querySelector(
+        ".player2ScoreContainer > .scoreCounterContainer2 > .scoreCounter"
+      );
       scoreCounter.textContent = parseInt(scoreCounter.textContent) + 1;
     }
   }
-  
-  function checkIfGameCanContinue(){
-    if(gameLogicHandler.hasSomeoneWon()){
+
+  function checkIfGameCanContinue() {
+    if (gameLogicHandler.hasSomeoneWon()) {
       playerHandler.displayWinner();
       increaseCounter();
       eventHandler.removeClickEventOnCells();
+      newRoundButton.classList.remove("invisible");
       return false;
-    }else if(!gameLogicHandler.areThereCellsLeft()){
+    } else if (!gameLogicHandler.areThereCellsLeft()) {
       eventHandler.removeClickEventOnCells();
       return false;
     }
     return true;
   }
 
-  const playRound = function(div) {
+  const playRound = function (div) {
     div.textContent = playerHandler.getActivePlayer().token;
-    if(!checkIfGameCanContinue()){
+    if (!checkIfGameCanContinue()) {
       /*handleHomeButton();*/
       return;
     }
     playerHandler.switchPlayerTurn();
-    console.log(
-      `${playerHandler.getActivePlayer().name}'s turn.(token = ${
-        playerHandler.getActivePlayer().token
-      })`
-    );
   };
-  const startDialog = ()=>{
-    console.log(
-      `${playerHandler.getActivePlayer().name}'s turn.(token = ${
-        playerHandler.getActivePlayer().token
-      })`
-      
-    );
-    divDisplayer.displayDiv.textContent=`${playerHandler.getActivePlayer().name}'s turn (${
-        playerHandler.getActivePlayer().token
-      })`
+  const startDialog = () => {
+    divDisplay.textContent = `${playerHandler.getFirstPlayer().name}'s turn (${
+      playerHandler.getFirstPlayer().token
+    })`;
   };
-  
+
   return {
     startDialog,
   };
 };
-const handleDisplayDiv = function(){
-  const displayDiv = document.querySelector(".display");
-  return{
-    displayDiv
-  }
+function getDisplayDiv() {
+  return document.querySelector(".display");
 }
-
-/*
-const handleHomeAndResetButton = function(){
-  const main = document.querySelector("main");
-  function showHomeButton(){
-    const homeButton = document.createElement("button");
-    homeButton.textContent = "Home";
-    homeButton.classList.add("homeButton");
-    main.appendChild(homeButton);
-    addEventListenerToHomeButton(homeButton);
-  }
-  function addEventListenerToHomeButton(homeButton){
-    homeButton.addEventListener("click",function(event){
-      document.body.classList.add("fade-out");
-      setTimeout(() => {
-        window.location.href = "index.html"; 
-      }, 300);
-    })
-  }
-
-  return{
-    showHomeButton
-  }
-}
-  */
 
 const player1 = createPlayer(sessionStorage.getItem("player1Name"), "X");
 const player2 = createPlayer(sessionStorage.getItem("player2Name"), "O");
